dd3cceb09a96b3629b8d69e2ea815b3006f07989
Austinps <aptstack@gmail.com>
2023-05-20 22:23:08 +0200

    update errorHanlder and 404 middleware in server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const { logger } = require("./src/middleware/logEvents");
+const notFound = require("./src/middleware/notFound");
 const errorHandler = require("./src/middleware/errorHandler");
 const verifyJWT = require("./src/middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
@@ -13,29 +14,15 @@ const mongoose = require("mongoose");
 const connectDB = require("./db/dbConnect");
 const PORT = process.env.PORT || 3500;
 
-// Connect to MongoDB
 connectDB();
 
-// custom middleware logger
 app.use(logger);
-
-// Handle options credentials check - before CORS!
-// and fetch cookies credentials requirement
 app.use(credentials);
-
-// Cross Origin Resource Sharing
 app.use(cors(corsOptions));
-
-// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
-
-// built-in middleware for json
 app.use(express.json());
-
-//middleware for cookies
 app.use(cookieParser());
 
-//serve static files
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
@@ -45,21 +32,11 @@ app.use("/auth", require("./src/routes/auth"));
 app.use("/refresh", require("./src/routes/refresh"));
 app.use("/logout", require("./src/routes/logout"));
 
-app.use(verifyJWT);
+// app.use(verifyJWT);
 app.use("/employees", require("./src/routes/api/employees"));
 app.use("/users", require("./src/routes/api/users"));
 
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ error: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
-});
-
+app.all("*", notFound);
 app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -7,3 +7,14 @@ const errorHandler = (err, req, res, next) => {
 };
 
 module.exports = errorHandler;
+
+const notFound = (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+};
